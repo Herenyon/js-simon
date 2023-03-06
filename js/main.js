0ff6abf5649e8f3c6792a8e274f52496dd1ff13b
Herenyon <rikitur17@gmail.com>
2023-03-06 23:06:29 +0100

    fine esercizio

--- js/main.js
+++ js/main.js
@@ -20,3 +20,58 @@ for (let i = 0; i < 5; i++) {
     board.append(element);
 
 }
+
+setTimeout(
+    function () {
+
+        board.classList.add('board-hide');
+
+
+    }
+
+
+
+    , 10000);
+
+
+setTimeout(
+    function () {
+        let numeriIndovinati = [];
+        for (let i = 0; i < 5; i++) {
+            let j = i + 1;
+            arrayUser[i] = Number(prompt("inserisci il numero" + ' ' + j ));
+
+        }
+
+        console.log(arrayUser)
+
+        let count = 0;
+        for (let i = 0; i <= arrayNumber.length - 1; i++) {
+
+            if (arrayNumber[i] === arrayUser[i]) {
+                count++;
+                numeriIndovinati[i] = arrayNumber[i];
+                
+                
+            }
+        
+        }
+
+        if (count === 1) {
+            console.log('hai indovinato' + ' ' + count + ' ' + 'numero');
+            console.log('Hai indovinato' + ' ' + numeriIndovinati);
+
+        }
+        else if (count > 1) {
+            console.log('hai indovinato' + ' ' + count + ' ' + 'numeri');
+            console.log('Hai indovinato' + ' ' + numeriIndovinati);
+
+        }
+
+        else {
+
+            console.log('non ne hai indovinato nessuno')
+        }
+
+    }
+    , 10100)
